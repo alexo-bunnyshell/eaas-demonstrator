2baace3b857fc9f5f68895094418ad24b32ecdb5
Alex Oprisan <iteratorx@lansator.ro>
2022-03-17 23:36:46 +0200

    show all secrets

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const app = express()
 
 app.get('/', function (req, res) {
     // console.log("req");
-    var message = "Bunnyshell ROCKS!\n<br>";
-    message += (process.env.APP_SECRET ? "I have a secret: "+process.env.APP_SECRET : "I don't have any secrets") 
+    var message = "Bunnyshell ROCKS!<br>";
+    message += (process.env.APP_SECRET ? "I have a secret: "+process.env.APP_SECRET : "I don't have any secrets") + "<br>"
+
+    Object.getOwnPropertyNames(process.env).forEach(
+        function(val, idx, array){
+            message+=val + process.env[val] + "<br>"
+            
+        }
+    )
+
 
     res.send(message)
 })
 
-
+Object.getOwnPropertyNames(process.env).forEach(
+    function(val, idx, array){
+        console.log(val + " = " + process.env[val])
+        
+    }
+)
 
     app.listen(3000, () => console.log('Server ready'))
