743b80734c8d40240083b20ae18b83fcfbec32cd
Alex Oprisan <iteratorx@lansator.ro>
2022-03-17 22:13:50 +0200

    show APP_SECRET in response

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Bunnyshell ROCKS!'))
-app.listen(3000, () => console.log('Server ready'))
+app.get('/', function (req, res) {
+    // console.log("req");
+    var message = "Bunnyshell ROCKS!\n<br>";
+    message += (process.env.APP_SECRET ? "I have a secret: "+process.env.APP_SECRET : "I don't have any secrets") 
+
+    res.send(message)
+})
+
+
+
+    app.listen(3000, () => console.log('Server ready'))
